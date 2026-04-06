fe0ffcfa500a211e6512693bc7081d03f266bf7c
Treasure Akanbi <treasureakanbi2019@gmail.com>
2026-04-06 22:12:23 +0100

    Fixed image paths and a typo in the Community section

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,13 @@ import Admission from './components/Admission';
 import Location from './components/Location';
 import ApplyNow from './components/ApplyNow';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App() {
   return (
     
     <div className="font-sans text-gray-800">
+      <Analytics />
         <Navbar />
         
       <Hero />
